54d7e662ef3fae6e251422836461ef4ed42e38b1
Martin Staňo <martinmatostano@gmail.com>
2020-11-15 18:16:13 +0100

    Add project files.

---
+++ Open-Lab-10.00/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_10._00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+        }
+    }
+}
