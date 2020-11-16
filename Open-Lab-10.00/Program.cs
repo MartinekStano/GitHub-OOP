eb2664265299f5abd101535d8ab7fa40822b4da0
Martin Staňo <martinmatostano@gmail.com>
2020-11-16 15:07:15 +0100

    Add more constructors with different variables

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -11,7 +11,7 @@ namespace Open_Lab_10._00
 
             
 
-            Console.Write("Zadaj nazov knihy: ");
+           /* Console.Write("Zadaj nazov knihy: ");
             string title = Console.ReadLine();
             
             Console.Write("Zadaj pocet stran: ");
@@ -39,6 +39,21 @@ namespace Open_Lab_10._00
             Book LOTR = new Book(title, pages, category, author, releaseDate);
 
             Console.WriteLine($"{LOTR.Category}\n{LOTR.Author}\n{LOTR.ReleaseDate}");
+           */
+            
+            Book LOTR2 = new Book();
+            Console.WriteLine(LOTR2.ToString());
+
+            Console.Write("Meno knihy: ");
+            string title2 = Console.ReadLine();
+            Console.Write("Pocet stran: ");
+            int pages2 = int.Parse(Console.ReadLine());
+
+            Book LOTR3 = new Book(title2, pages2);
+            Console.WriteLine(LOTR3.ToString());
+
+            Book HOBIT = new Book("Hobit", 232, "SCI-FI", "J.R.R. Tolkien", 2012);
+            Console.WriteLine(HOBIT.ToString());
             
             
 
@@ -62,9 +77,30 @@ namespace Open_Lab_10._00
             Author = author;
             ReleaseDate = releasedate;
         }
+        public Book()
+        {
+            Title = "-1";
+            Pages = -1;
+            Category = "-1";
+            Author = "-1";
+            ReleaseDate = -1;
+        }
+        public Book(string title, int pages)
+        {
+            Title = title;
+            Pages = pages;
+            Category = "-1";
+            Author = "-1";
+            ReleaseDate = -1;
 
-
+        }
+        public override string ToString()
+        {
+            return $"{Title}\n{Pages}\n{Category}\n{Author}\n{ReleaseDate}";
+        }
         
+
+
         public void DiscribeBook()
         {
             Console.WriteLine($"{Title} {Pages} {Category} {Author} {ReleaseDate}");
