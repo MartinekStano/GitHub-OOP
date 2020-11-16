ee038efe985243dad04197c5ae34439c23e501af
Martin Staňo <martinmatostano@gmail.com>
2020-11-16 17:11:22 +0100

    Add public static list

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Open_Lab_10._00
 {
@@ -6,68 +7,82 @@ namespace Open_Lab_10._00
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
+            /* Console.Write("Zadaj nazov knihy: ");
+             string title = Console.ReadLine();
 
+             Console.Write("Zadaj pocet stran: ");
+             int pages =int.Parse(Console.ReadLine());
+
+             if(pages < 0)
+             {
+                 pages = 1;
+             }
+
+             Console.Write("Zadaj zaner: ");
+             string category = Console.ReadLine();
+
+             Console.Write("Zadaj meno autora: ");
+             string author = Console.ReadLine();
+
+             Console.Write("Zadaj datum vydania (rok): ");
+             int releaseDate = int.Parse(Console.ReadLine());
+
+             if (releaseDate <= 1450 || releaseDate >= 2021)
+             {
+                 releaseDate = -1;
+             }
+
+             Book LOTR = new Book(title, pages, category, author, releaseDate);
+
+             Console.WriteLine($"{LOTR.Category}\n{LOTR.Author}\n{LOTR.ReleaseDate}");
+            */
+
+            /* Book LOTR2 = new Book();
+             Console.WriteLine(LOTR2.ToString());
+
+             Console.Write("Meno knihy: ");
+             string title2 = Console.ReadLine();
+             Console.Write("Pocet stran: ");
+             int pages2 = int.Parse(Console.ReadLine());
+
+             Book LOTR3 = new Book(title2, pages2);
+             Console.WriteLine(LOTR3.ToString());
+            */
+
+            List<string> gerne = new List<string>();
             
+            gerne.Add("detské");
+            gerne.Add("romantické");
+            gerne.Add("náučné");
+            gerne.Add("sci-fi");
+            gerne.Add("dobrodružné");
 
-           /* Console.Write("Zadaj nazov knihy: ");
-            string title = Console.ReadLine();
-            
-            Console.Write("Zadaj pocet stran: ");
-            int pages =int.Parse(Console.ReadLine());
+            Book.CategoryList = gerne;
 
-            if(pages < 0)
-            {
-                pages = 1;
-            }
+            string category = Book.ChooseAGenre();
 
-            Console.Write("Zadaj zaner: ");
-            string category = Console.ReadLine();
-            
-            Console.Write("Zadaj meno autora: ");
-            string author = Console.ReadLine();
-            
-            Console.Write("Zadaj datum vydania (rok): ");
-            int releaseDate = int.Parse(Console.ReadLine());
-           
-            if (releaseDate <= 1450 || releaseDate >= 2021)
-            {
-                releaseDate = -1;
-            }
-
-            Book LOTR = new Book(title, pages, category, author, releaseDate);
-
-            Console.WriteLine($"{LOTR.Category}\n{LOTR.Author}\n{LOTR.ReleaseDate}");
-           */
-            
-            Book LOTR2 = new Book();
-            Console.WriteLine(LOTR2.ToString());
-
-            Console.Write("Meno knihy: ");
-            string title2 = Console.ReadLine();
-            Console.Write("Pocet stran: ");
-            int pages2 = int.Parse(Console.ReadLine());
-
-            Book LOTR3 = new Book(title2, pages2);
-            Console.WriteLine(LOTR3.ToString());
-
-            Book HOBIT = new Book("Hobit", 232, "SCI-FI", "J.R.R. Tolkien", 2012);
-            Console.WriteLine(HOBIT.ToString());
-            
-            
-
-            
+            Book HOBIT = new Book("Hobit", 232,category, "J.R.R. Tolkien", 2012);
+            HOBIT.DiscribeBook();
         }
-       
+
+
+
+
+
+
+            
+            
+        
+
     }
     class Book
     {
         private string Title { get; set; }
         private int Pages { get; set; }
-        public string Category { get; set; }
+        public string Category { private get; set; }
         public string Author { get; set; }
         public int ReleaseDate { get; set; }
+        public static List<string> CategoryList { get; set; }
 
         public Book(string title, int pages, string categoty, string author, int releasedate)
         {
@@ -76,7 +91,9 @@ namespace Open_Lab_10._00
             Category = categoty;
             Author = author;
             ReleaseDate = releasedate;
+            CategoryList = new List<string>();
         }
+        
         public Book()
         {
             Title = "-1";
@@ -98,13 +115,40 @@ namespace Open_Lab_10._00
         {
             return $"{Title}\n{Pages}\n{Category}\n{Author}\n{ReleaseDate}";
         }
-        
-
-
         public void DiscribeBook()
         {
             Console.WriteLine($"{Title} {Pages} {Category} {Author} {ReleaseDate}");
-           
         }
+        
+        
+        public static string ChooseAGenre()
+        {
+            int i = 1;
+            foreach (string genre in Book.CategoryList)
+            {
+                
+                Console.WriteLine("{0} {1}", i, genre);
+                i += 1;
+            }
+            Console.WriteLine("Tvoj vyber: ");
+            string category = Book.CategoryList[int.Parse(Console.ReadLine())-1];
+            return category;
+        }
+        
+
+           
+        
+
+
+
+
+
+
+        
+        
+        
+
+
+
     }
 }
